2ff87b34645f8abebabf364a6f407b93ce926f47
alvioladagreat <carlsenhikaru399473@gmail.com>
2025-11-09 22:43:39 +0800

    Update StackRenderStrategy.java

--- src/main/java/com/quiboysstudio/quicards/states/matchmaking/StackRenderStrategy.java
+++ src/main/java/com/quiboysstudio/quicards/states/matchmaking/StackRenderStrategy.java
@@ -1,1 +1,8 @@
+package com.quiboysstudio.quicards.states.matchmaking;
 
+import java.awt.Graphics2D;
+import java.util.List;
+
+interface StackRenderStrategy {
+    void render(Graphics2D g2d, List<CardComponent> cards, int x, int y, int width, int height);
+}
